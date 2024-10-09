e7d20f448a5e21fb2a48f90e3cdcb105aac5133e
Mingzhang Deng <dengmingzhang2002@outlook.com>
2024-10-09 13:51:23 +0800

    add neovim, ncdf

--- nixos/default/default.nix
+++ nixos/default/default.nix
@@ -24,6 +24,7 @@ in
 
         #vim
         pkgs.vim
+	pkgs.neovim
 
         #media
         pkgs.vlc
@@ -54,6 +55,7 @@ in
 
         #tools
         pkgs.localsend
+        pkgs.ncdu
 
         #games
         pkgs.steam
